5c014005c730c23803e14475f5aeb24980999d67
Dima <dziurziadmytro@gmail.com>
2024-02-05 23:11:15 +0200

    validation && logging

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { Logger } from 'nestjs-pino';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+
+  app.useLogger(app.get(Logger));
+
+  const configService = app.get(ConfigService);
+  await app.listen(configService.getOrThrow('PORT'));
 }
 bootstrap();
